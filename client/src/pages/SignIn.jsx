3969a5d45cbe7b4f369bbd7aa29e344104ce1668
esiepe <obwangitowen@gmail.com>
2023-11-29 17:09:30 +0300

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from "react-router-dom"
-
+import { useDispatch, useSelector } from 'react-redux'
+import { 
+    signInStart, 
+    signInSuccess, 
+    signInFailure,
+} from '../redux/user/userSlice'
 
 export default function SignIn() {
   const [formData, setFormData] = useState({})
-  const [error, setError] = useState(null)
-  const [loading, setLoading] = useState(false)
+  const { loading, error } = useSelector((state) => state.user)
   const navigate = useNavigate()
+  const dispatch = useDispatch()
   const handleChange = (e) => {
     setFormData(
       {
@@ -18,26 +23,23 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault()
     try {
-      setLoading(true)
-    const res = await fetch("/api/auth/signin", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    })
-    const data = await res.json()
-    if (data.success === false) {
-      setError(data.message)
-      setLoading(false)
-      return
-    }
-    setLoading(false)
-    setError(null)
-    navigate("/")
+      dispatch(signInStart())
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      })
+      const data = await res.json()
+      if (data.success === false) {
+        dispatch(signInFailure(data.message))
+        return
+      }
+      dispatch(signInSuccess(data))
+      navigate("/")
     } catch (error) {
-      setLoading(false)
-      setError(error.message)
+      dispatch(signInFailure(data.message))
     }
     
   }
---
+++ client/src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit"
+import userReducer from "./user/userSlice.js"
+export const store = configureStore({
+    reducer: {user: userReducer},
+    middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware({
+        serializableCheck: false,
+    }),
+})
