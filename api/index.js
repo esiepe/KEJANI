688f366d335f905981f18aeea3fa812317c1247d
esiepe <obwangitowen@gmail.com>
2024-03-23 01:09:50 +0300

    fixed path again

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
 import listingRouter from "./routes/listing.route.js"
 import cookieParser from "cookie-parser"
+import path from "path"
 dotenv.config()
 
 mongoose
