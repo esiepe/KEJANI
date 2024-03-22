24c0132035e8d4ec9a17abe223d2d6a01f8c4755
esiepe <obwangitowen@gmail.com>
2024-03-23 00:26:36 +0300

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Kejani Estate</h1>
+      <p className='mb-4 text-slate-700'>
+        KejaniEstate is a pioneering real estate marketplace designed exclusively for Kabarak University students seeking accommodation. Our platform offers a comprehensive solution for students to discover, explore, and secure rental properties within the university vicinity. With a commitment to excellence and efficiency, KejaniEstate aims to redefine the student housing experience.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Our mission at KejaniEstate is simple: to empower Kabarak University students in their search for the perfect accommodation. We strive to provide personalized service, expert guidance, and unparalleled access to a wide range of rental properties. Whether you're a student in search of a cozy apartment or a landlord looking to showcase your property, KejaniEstate is your trusted partner every step of the way.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Led by a team of dedicated professionals with a deep understanding of the student housing market, KejaniEstate is committed to delivering exceptional service to our users. We believe that finding the right accommodation should be an exciting and fulfilling journey, and we are passionate about making that vision a reality for Kabarak University students.
+      </p>
+    </div>
   )
 }
